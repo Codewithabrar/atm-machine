6cc304f7584b3541213b53a8379f2ee792295699
Codewithabrar <162000914+Codewithabrar@users.noreply.github.com>
2024-04-26 00:08:50 +0500

    adding of some useful tool
     i add in this version fast cash and some most useful tool or restrictions

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let myAmount = 1000;
+let myAmount = 100000;
 const myPin = 5555;
 let pinAnswer = await inquirer.prompt([
     {
         name: "pinQuestion",
         message: "Enter your pinCode",
-        type: "number"
-    }
+        type: "number",
+    },
 ]);
 if (pinAnswer.pinQuestion === myPin) {
     let operationAnswer = await inquirer.prompt([
@@ -15,7 +15,7 @@ if (pinAnswer.pinQuestion === myPin) {
             name: "operation",
             message: "Select any one option",
             type: "list",
-            choices: ["withdraw", "balance inquiry", "fastCash",]
+            choices: ["withdraw", "balance inquiry", "fastCash"],
         }
     ]);
     if (operationAnswer.operation === "fastCash") {
@@ -24,31 +24,36 @@ if (pinAnswer.pinQuestion === myPin) {
                 name: "exactAmount",
                 message: "please select any amount.",
                 type: "list",
-                choices: ['500', '1000', '2000', '3000', '4000', '5000', '6000']
-            }
+                choices: ["500", "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000", "10000"],
+            },
         ]);
         if (fastCash.exactAmount >= myAmount) {
             console.log("low balance you can not withdraw");
         }
         else if (fastCash.exactAmount < myAmount) {
             myAmount -= fastCash.exactAmount;
-            console.log("Remaining balance in your account is: " + myAmount);
+            console.log("Thank You for using this ATM");
         }
     }
     if (operationAnswer.operation === "withdraw") {
         let amountAnswer = await inquirer.prompt([
             {
                 name: "amount",
-                message: "Enter your amount",
-                type: "number"
+                message: "Enter multiple of 500",
+                type: "number",
             }
         ]);
+        if (amountAnswer.amount % 500 === 0 && amountAnswer.amount <= 25000) {
+            console.log("Thank You for using this ATM");
+        }
+        else if (amountAnswer.amount % 500 != 0) {
+            console.log("sorry we can not proceed your tansetion please enter a valid amount");
+        }
+        else if (amountAnswer.amount > 25000) {
+            console.log("sorry we can not proceed your tansetion please enter a valid amount");
+        }
         if (amountAnswer.amount >= myAmount) {
             console.log("low balance you can not withdraw");
-        }
-        else if (amountAnswer.amount < myAmount) {
-            myAmount -= amountAnswer.amount;
-            console.log("Remaining balance in your account is: " + myAmount);
         }
     }
     else if (operationAnswer.operation === "balance inquiry") {
@@ -58,4 +63,3 @@ if (pinAnswer.pinQuestion === myPin) {
 else {
     console.log("incorrect pin code");
 }
-;
--- main.ts
+++ main.ts
@@ -1,76 +1,70 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let myAmount = 1000;
+let myAmount = 100000;
 const myPin = 5555;
-let pinAnswer = await inquirer.prompt(
-    [
-        {
-            name: "pinQuestion",
-            message: "Enter your pinCode",
-            type: "number"
-        }
-    ]
-);
-if(pinAnswer.pinQuestion=== myPin){
-   
-let operationAnswer = await inquirer.prompt(
-    [
-        {
-            name: "operation",
-            message: "Select any one option",
-            type: "list",
-            choices: ["withdraw","balance inquiry","fastCash",]
-        }
-    ]
-);
-
-if(operationAnswer.operation === "fastCash"){
-let fastCash = await inquirer.prompt(
-    [
-        {
-          name: "exactAmount",
-          message: "please select any amount.",
-          type: "list",
-          choices:['500','1000','2000','3000','4000','5000','6000']  
-        }
-    ]
-);
-if(fastCash.exactAmount >= myAmount){
-    console.log("low balance you can not withdraw")}
-    else if (fastCash.exactAmount < myAmount){
-        myAmount -= fastCash.exactAmount;
-        console.log("Remaining balance in your account is: " + myAmount);
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pinQuestion",
+    message: "Enter your pinCode",
+    type: "number",
+  },
+]);
+if (pinAnswer.pinQuestion === myPin) {
+  let operationAnswer = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "Select any one option",
+      type: "list",
+      choices: ["withdraw", "balance inquiry", "fastCash"],
     }
-}
+]
+);
 
-if(operationAnswer.operation === "withdraw"){
-    let amountAnswer = await inquirer.prompt(
-        [
-            {
-                name: "amount",
-                message: "Enter your amount",
-                type: "number"
-            }
-        ]
-    );
-    
+  if (operationAnswer.operation === "fastCash") {
+    let fastCash = await inquirer.prompt([
+      {
+        name: "exactAmount",
+        message: "please select any amount.",
+        type: "list",
+        choices: ["500", "1000", "2000", "3000", "4000", "5000", "6000","7000","8000","9000","10000"],
+      },
+    ]);
+    if (fastCash.exactAmount >= myAmount) {
+      console.log("low balance you can not withdraw");
+    } else if (fastCash.exactAmount < myAmount) {
+      myAmount -= fastCash.exactAmount;
+      console.log("Thank You for using this ATM");
+    }
+  }
 
-    if(amountAnswer.amount >= myAmount){
-        console.log("low balance you can not withdraw")}
-        else if (amountAnswer.amount < myAmount){
-            myAmount -= amountAnswer.amount;
-            console.log("Remaining balance in your account is: " + myAmount);
-        }
+  if (operationAnswer.operation === "withdraw"){
+    let amountAnswer = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Enter multiple of 500",
+        type: "number",
+      }
+    ]);
+      
+    if (amountAnswer.amount % 500 === 0 && amountAnswer.amount <= 25000 ) {
+      
+      console.log("Thank You for using this ATM");
+    } else if(amountAnswer.amount % 500 != 0) {
        
-   
-}
-    else if(operationAnswer.operation ==="balance inquiry"){
-        console.log("Your current balance is :" + myAmount);
+      console.log(
+        "sorry we can not proceed your tansetion please enter a valid amount"
+      );
+    } else if(amountAnswer.amount >25000){
+      console.log("sorry we can not proceed your tansetion please enter a valid amount")
     }
 
+    if (amountAnswer.amount >= myAmount) {
+      console.log("low balance you can not withdraw");
+    }
     
+  } else if (operationAnswer.operation === "balance inquiry") {
+    console.log("Your current balance is :" + myAmount);
+  }
+} else {
+  console.log("incorrect pin code");
 }
-else{
-    console.log("incorrect pin code")
-};
-
